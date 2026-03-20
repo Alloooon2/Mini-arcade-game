b14401e0d76e7a13c1f3a08a46d27959617b259e
Allen Lei <allenlei@Mac.aquinas.wa.edu.au>
2026-03-20 13:39:35 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def load_values ():
+    values = []
+    for i in range (5):
+        num = int(inpt("Enter a number: "))
+        values.append(num)
+    return values
---
+++ mini arcade code.py
@@ -0,0 +1,28 @@
+import random
+import time
+
+
+print("1. Games")
+time.sleep(1)
+print("2. Score")
+time.sleep(1)
+print("3. Quit")
+time.sleep(1)
+menu_select = input("Enter your choice (1, 2, 3): ")
+
+while True:
+    if menu_select == "1":
+        print("1. Rock, paper scissors")
+        time.sleep(1)
+        print("2. Hangman")
+        games = input("Pick a game to play: ")
+        if games == "1":
+            print("Hangman")
+        elif games == "2":
+            print("Rock, paper, scissors")
+        elif games == "3":
+            print("Going back to menu")
+        else:
+            print("Invalid choice, pick again")
+    elif menu_select == "2":
+        print("Scoreboard")
